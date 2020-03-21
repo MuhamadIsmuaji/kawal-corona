a1ba2be71a6d543a3326cec357ccd64d6a4520bf
ekhel <blackmanskill@gmail.com>
2020-03-21 15:22:09 +0900

    Initial

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# kawal-corona
+Scrapping
---
+++ src/components/App.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react';
+import kawalcorona from '../api/kawalcorona';
+import CoronaData from './CoronaData';
+//import CoronaData from './CoronaData';
+
+class App extends React.Component {
+  state = { positif: [] }
+
+  DataCorona = async () => {
+    const response = await kawalcorona.get('/positif');
+    
+    this.setState({ positif: response.data.results });
+  }
+  
+  render() {
+    return (
+      <div>
+        <Menu fixed='top' inverted>
+          <Container>
+            <Menu.Item as='a' header>
+              <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
+              BURKAS INFO
+            </Menu.Item>
+            <Menu.Item as='a'>Home</Menu.Item>
+
+            <Dropdown item simple text='Dropdown'>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Header>Header Item</Dropdown.Header>
+                <Dropdown.Item>
+                  <i className='dropdown icon' />
+                  <span className='text'>Submenu</span>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item>List Item</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Container>
+        </Menu>
+
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>Life Kawal Korona Virus</Header>
+          
+          <CoronaData items={this.state.items}/>
+          
+        </Container>
+
+        <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+          <Container textAlign='center'>
+            <Grid divided inverted stackable>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 1' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 2' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 3' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header inverted as='h4' content='Footer Header' />
+                <p>
+                  Extra space for a call to action inside the footer that could help re-engage users.
+                </p>
+              </Grid.Column>
+            </Grid>
+
+            <Divider inverted section />
+            <Image centered size='mini' src='/logo.png' />
+            <List horizontal inverted divided link size='small'>
+              <List.Item as='a' href='#'>
+                Site Map
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Contact Us
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Terms and Conditions
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Privacy Policy
+              </List.Item>
+            </List>
+          </Container>
+        </Segment>
+  </div>
+    )
+  }
+
+}
+
+export default App;
