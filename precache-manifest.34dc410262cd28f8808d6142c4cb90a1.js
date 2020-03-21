ed692385aa02cf417973e1a34b9dbc5940f52523
ekhel <blackmanskill@gmail.com>
2020-03-21 18:33:04 +0900

    Updates

---
+++ precache-manifest.34dc410262cd28f8808d6142c4cb90a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c674ca7b18f33d27c4fecfe9b4d528",
+    "url": "/kawal-corona/index.html"
+  },
+  {
+    "revision": "d443696a6e4b9635f160",
+    "url": "/kawal-corona/static/css/main.f8fc89c8.chunk.css"
+  },
+  {
+    "revision": "2e811b5818dd9a39cc77",
+    "url": "/kawal-corona/static/js/2.0f13f641.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kawal-corona/static/js/2.0f13f641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d443696a6e4b9635f160",
+    "url": "/kawal-corona/static/js/main.b12db1fa.chunk.js"
+  },
+  {
+    "revision": "32844da8dc9a6feb4b53",
+    "url": "/kawal-corona/static/js/runtime-main.61de4508.js"
+  }
+]);
